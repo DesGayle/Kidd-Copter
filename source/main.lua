6498fa0df8dd066df417b427f8897a466021c5ba
Des <des@alteredgene.co.uk>
2025-04-05 23:27:07 +0100

    Project setup and displaying the player on screen.

---
+++ source/main.lua
@@ -0,0 +1,12 @@
+local pd = playdate
+local gfx = pd.graphics
+
+
+-- Player
+local playerX = 40
+local playerY = 120
+local playerImage = gfx.image.new("images/chopper")
+
+function pd.update()
+    playerImage:draw(playerX, playerY)
+end
